9b45789b241df3029e1336fa45bae22259a60930
Hayleyesus Gujo <12hayleloke@gmail.com>
2023-10-04 12:49:25 -0700

    add image

---
+++ Introduce yourself/script.js
@@ -0,0 +1,4 @@
+var firstName = "FirstName";
+var lastName = "LastName";
+var yearsOfStudy = 2; // Update with your years of study
+var goal = "To find a job related to  software engineer."; // Update with your goal
